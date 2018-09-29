8be5a0b3d69514ec777d12912bb206cdb742ed35
Hx76 <947769582@qq.com>
2018-09-29 09:50:54 +0800

    sort
    10个数排序

--- sort/main.swift
+++ sort/main.swift
@@ -7,6 +7,29 @@
 //
 
 import Foundation
+var a = [1,22,33,44,55,0,7,8,9,10]
+var i=0
+var j=9
+var temp : Int
+for i in 0..<10
+{
+    while j>i
+    {
+        if a[i]>a[j]
+        {
+            temp=a[i]
+            a[i]=a[j]
+            a[j]=temp
+        }
+        j=j-1
+    }
+    j=9
+}
+i=0
+while i<10
+{
+    print(a[i])
+    i=i+1
+}
 
-print("Hello, World!")
 
